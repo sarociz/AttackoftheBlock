e4520820e245757c2523ba86540ad08a83313be6
Sara <sara.ocasar@ikasle.egibide.org>
2024-09-17 20:53:42 +0200

    Movimiento del personaje con el mouse

--- Assets/Code/MouseMovement.cs
+++ Assets/Code/MouseMovement.cs
@@ -3,51 +3,22 @@ using System.Collections.Generic;
 using UnityEngine;
 
 public class MouseMovement : MonoBehaviour
-{
-    public float speed = 10f;
-    public Vector3 targetPos;
-    public bool isMoving;
-    const int MOUSE = 0;
+{    
+
     // Use this for initialization1
     void Start()
     {
+       
 
-        targetPos = transform.position;
-        isMoving = false;
     }
 
     // Update is called once per frame
     void Update()
     {
-
-        if (Input.GetMouseButton(MOUSE))
-        {
-            SetTarggetPosition();
-        }
-        if (isMoving)
-        {
-            MoveObject();
-        }
+        Vector2 mousePosition = Camera.main.ScreenToWorldPoint(Input.mousePosition);
+        //mousePosition.z = Camera.main.transform.position.z + Camera.main.nearClipPlane;
+        //transform.position = Vector2.MoveTowards(transform.position, mousePosition, speed * Time.deltaTime);
+        transform.position = mousePosition;
     }
-    void SetTarggetPosition()
-    {
-        Plane plane = new Plane(Vector3.up, transform.position);
-        Ray ray = Camera.main.ScreenPointToRay(Input.mousePosition);
-        float point = 0f;
 
-        if (plane.Raycast(ray, out point))
-            targetPos = ray.GetPoint(point);
-
-        isMoving = true;
-    }
-    void MoveObject()
-    {
-        transform.LookAt(targetPos);
-        transform.position = Vector3.MoveTowards(transform.position, targetPos, speed * Time.deltaTime);
-
-        if (transform.position == targetPos)
-            isMoving = false;
-        Debug.DrawLine(transform.position, targetPos, Color.red);
-
-    }
 }
