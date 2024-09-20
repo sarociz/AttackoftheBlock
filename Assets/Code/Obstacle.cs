f2c890003ad017be5cfa5b15d772fa7e467c4769
Sara <sara.ocasar@ikasle.egibide.org>
2024-09-20 17:10:55 +0200

    Pantalla Game over + collision bloque-bola + rebote bloque con límites

--- Assets/Code/Obstacle.cs
+++ Assets/Code/Obstacle.cs
@@ -5,12 +5,41 @@ using UnityEngine.SceneManagement;
 
 public class Obstacle : MonoBehaviour
 {
-
-    private void OnCollisionEnter2D(Collision2D collision)
+    public Rigidbody2D rbObstaculo;
+    public float rapidez = 300;
+    private Vector2 velocidad;
+    private void Start()
     {
+        rbObstaculo.velocity = Vector2.zero;
+        velocidad.x = Random.Range(-1f, 1f);
+        velocidad.y = -1;
+        rbObstaculo.AddForce(velocidad.normalized * rapidez);
+    }
+
+    private void OnCollisionEnter2D(Collision2D collision){
         if (collision.gameObject.CompareTag("Personaje"))
         {
             SceneManager.LoadScene("GameOver");
+            //Application.Quit();
+        }
+        velocityFix();
+    }
+
+    private void velocityFix()
+    {
+        float velocityDelta = 0.5f;
+        float minVelocity = 0.2f;
+
+        if (Mathf.Abs(rbObstaculo.velocity.x) < minVelocity)
+        {
+            velocityDelta = Random.value < 0.5f ? velocityDelta : -velocityDelta;
+            rbObstaculo.velocity += new Vector2(velocityDelta, 0f);
+        }
+
+        if (Mathf.Abs(rbObstaculo.velocity.y) < minVelocity)
+        {
+            velocityDelta = Random.value < 0.5f ? velocityDelta : -velocityDelta;
+            rbObstaculo.velocity += new Vector2(velocityDelta, 0f);
         }
     }
 }
